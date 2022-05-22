ec5ee5b775a6376d1c2d4591e5199e8e42127657
Julio3103 <106038544+Julio3103@users.noreply.github.com>
2022-05-22 18:17:53 -0500

    Initial commit

---
+++ app/src/main/java/com/pluartz/hj_informa/MainActivity.java
@@ -0,0 +1,14 @@
+package com.pluartz.hj_informa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
